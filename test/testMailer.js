bcca8339c48bad540181f359ca80724bc1988dc5
guimaodai <wyyxdgm@163.com>
2016-03-08 10:53:30 +0800

    test

--- test/testMailer.js
+++ test/testMailer.js
@@ -7,21 +7,21 @@ var transporter = nodemailer.createTransport({
     "secure": true,
     "port": 465,
     "auth": {
-        "user": "wyyxdgm@163.com",
-        "pass": "228209"
+        "user": "testmailer001@163.com",
+        "pass": "testmailer"
     }
 });
 
 // setup e-mail data with unicode symbols
 var mailOptions = {
-    from: '"Fred Foo " <wyyxdgm@163.com>', // sender address
-    to: 'jiekuandgm@163.com, wyyxdgm@163.com', // list of receivers
+    from: '"Fred Foo " <jiekuandgm@163.com>', // sender address
+    to: 'jiekuandgm@163.com', // list of receivers
     subject: 'Hello ', // Subject line
     text: 'Hello world ', // plaintext body
     html: '<p style="color:blue;">Hello world </p><b>Hello world </b>', // html body
     "attachments": [{
-        "filename": "app.js",
-        "path": "./app.js"
+        "filename": "testMailer.js",
+        "path": __dirname + "/testMailer.js"
     }]
 };
 
