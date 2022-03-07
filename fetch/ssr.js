6a2099fe9e3c2449cb9032ebc22be1d8a24934f7
Damo <daiguimao@sightp.com>
2022-03-07 10:34:32 +0800

    configs

--- fetch/ssr.js
+++ fetch/ssr.js
@@ -1,16 +1,54 @@
-var s2 = document.querySelectorAll("#post-box > div > section > div > table > tbody > tr")
-var ss_LinkList = [];
-for (var i = 0; i < s2.length; i++) {
-  //console.log('ss://'+s2[i].children[4].innerText+':'+s2[i].children[3].innerText+'@'+s2[i].children[1].innerText+':'+s2[i].children[2].innerText);
-  ss_LinkList.push({
-    method: s2[i].children[4].innerText,
-    password: s2[i].children[3].innerText,
-    server: s2[i].children[1].innerText,
-    server_port: Number(s2[i].children[2].innerText),
-    obfs: s2[i].children[6].innerText,
-    protocol: s2[i].children[5].innerText,
+// ssr
+var ssrTr = document.querySelectorAll("#post-box > div > section > div > table > tbody > tr")
+var ssr_LinkList = [];
+for (var i = 0; i < ssrTr.length; i++) {
+  //console.log('ss://'+ssrTr[i].children[4].innerText+':'+ssrTr[i].children[3].innerText+'@'+ssrTr[i].children[1].innerText+':'+ssrTr[i].children[2].innerText);
+  ssr_LinkList.push({
+    method: ssrTr[i].children[4].innerText,
+    password: ssrTr[i].children[3].innerText,
+    server: ssrTr[i].children[1].innerText,
+    server_port: Number(ssrTr[i].children[2].innerText),
+    obfs: ssrTr[i].children[6].innerText,
+    protocol: ssrTr[i].children[5].innerText,
     enable: true,
-    remarks: s2[i].children[1].innerText
+    remarks: ssrTr[i].children[1].innerText
   });
 }
-console.log(JSON.stringify(ss_LinkList))
+let ssr_obj = {
+  "random" : false,
+  "authPass" : null,
+  "useOnlinePac" : false,
+  "TTL" : 0,
+  "global" : false,
+  "reconnectTimes" : 3,
+  "index" : 0,
+  "proxyType" : 0,
+  "proxyHost" : null,
+  "authUser" : null,
+  "proxyAuthPass" : null,
+  "isDefault" : false,
+  "pacUrl" : null,
+  "configs" : [
+    {
+      "enable" : true,
+      "password" : "dongtaiwang.com",
+      "method" : "none",
+      "remarks" : "新服务器",
+      "server" : "lg1.free4444.xyz",
+      "obfs" : "tls1.2_ticket_auth",
+      "protocol" : "auth_chain_a",
+      "server_port" : 443,
+      "remarks_base64" : "5paw5pyN5Yqh5Zmo"
+    }
+  ],
+  "proxyPort" : 0,
+  "randomAlgorithm" : 0,
+  "proxyEnable" : false,
+  "enabled" : true,
+  "autoban" : false,
+  "proxyAuthUser" : null,
+  "shareOverLan" : false,
+  "localPort" : 1080
+}
+ssr_obj.configs = ssr_obj.configs.concat(ssr_LinkList);
+console.log(JSON.stringify(ssr_obj))
